1474d19aed98bdc17d7d7cc4bb8147752548ab79
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-06-28 13:13:31 -0300

    feat:criando pipe de erro do zod

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -1,13 +1,30 @@
-import { Body, ConflictException, Controller, Post } from "@nestjs/common";
+import {
+  Body,
+  ConflictException,
+  Controller,
+  Post,
+  UsePipes,
+} from "@nestjs/common";
 import { hash } from "bcryptjs";
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
 import { PrismaService } from "src/prisma/prisma.service";
+import { z } from "zod";
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>;
 
 @Controller("/accounts")
 export class CreateAccountController {
   constructor(private prisma: PrismaService) {}
 
   @Post()
-  async handler(@Body() body: any) {
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handler(@Body() body: CreateAccountBodySchema) {
     const { name, email, password } = body;
     const userWithSameEmail = await this.prisma.user.findUnique({
       where: {
