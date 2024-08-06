3350b699ab6a82a3bba47165a7d0bc887d572524
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-08-06 20:32:04 -0300

    presenter de question

--- src/infra/http/controllers/fetch-recent-questions.controller.ts
+++ src/infra/http/controllers/fetch-recent-questions.controller.ts
@@ -5,6 +5,7 @@ import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { z } from 'zod';
 import { ZodValidationPipe } from '../pipes/zod-validation-pipe';
 import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions';
+import { QuestionPresenter } from '../presenters/question-presenter';
 
 const pageQueryParamSchema = z
   .string()
@@ -26,10 +27,15 @@ export class FetchRecentQuestionsController {
   async handler(
     @Query('page', queryValidationPipe) page: PageQueryParamSchema,
   ) {
-    const questions = await this.fetchRecentQuestionsUseCase.execute({
+    const result = await this.fetchRecentQuestionsUseCase.execute({
       page
     })
 
-    return { questions };
+    if (result.isLeft()) { 
+      throw new Error('error');
+    }
+
+    const questions = result.value.questions
+    return { questions:questions.map(QuestionPresenter.toHTTP) };
   }
 }
