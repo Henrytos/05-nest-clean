917663537d6d06980e606f57bce8dd3dd38fcedb
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-08-08 18:48:33 -0300

    refactor: remove unused imports and update question commenting logic

--- src/domain/forum/application/use-cases/comment-on-question.ts
+++ src/domain/forum/application/use-cases/comment-on-question.ts
@@ -1,5 +1,4 @@
 import { UniqueEntityID } from '@/core/entities/unique-entity-id';
-import { Question } from '../../enterprise/entities/question';
 import { QuestionsRepository } from '../repositories/question-repository';
 import { QuestionCommentsRepository } from '../repositories/question-comments-repository';
 import { QuestionComment } from '../../enterprise/entities/question-comment';
