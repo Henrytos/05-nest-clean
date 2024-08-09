ccd76141f0165bb807fc7a4b87ab5f4c4a1c8128
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-08-09 19:49:53 -0300

    refactor: Update PrismaQuestionAttachmentMapper and PrismaService usage in makePrismaQuestionAttachements method

--- test/factories/make-question-attachment.ts
+++ test/factories/make-question-attachment.ts
@@ -3,6 +3,8 @@ import {
   QuestionAttachment,
   QuestionAttachmentProps,
 } from '@/domain/forum/enterprise/entities/question-attachment';
+import { PrismaQuestionAttachmentMapper } from '@/infra/database/prisma/mappers/prisma-question-attachment-mapper';
+import { PrismaService } from '@/infra/database/prisma/prisma.service';
 
 export function makeQuestionAttachment(
   override: Partial<QuestionAttachmentProps> = {},
@@ -18,4 +20,20 @@ export function makeQuestionAttachment(
   );
 
   return questionAttachment;
+}
+
+export class QuestionAttachmentFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaQuestionAttachements(data: Partial<QuestionAttachmentProps>) {
+    const questionAttachment = makeQuestionAttachment(data);
+
+    /*
+        await this.prisma.attachment.create({
+      data: PrismaQuestionAttachmentMapper,
+    });
+
+    return questionAttachment;
+    */
+  }
 }
