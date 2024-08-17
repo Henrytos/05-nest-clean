fe6edb3e8033a020e8d7cb6bc878da3ce6aec8d6
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-08-17 10:23:29 -0300

    chore: Update npm dependency to latest stable version

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,22 @@
 import swc from 'unplugin-swc';
 import { defineConfig } from 'vitest/config';
-import tsConfigPaths from 'vitest-tsconfig-paths'
+import tsConfigPaths from 'vitest-tsconfig-paths';
 
 export default defineConfig({
   test: {
     globals: true,
     root: './',
+    coverage: {
+      provider: 'istanbul',
+      reportsDirectory: './src/domain/forum/aplication/use-cases',
+      reporter: [
+        // Specify reporter using name of the NPM package
+        ['@vitest/custom-coverage-reporter', { someOption: true }],
+
+        // Specify reporter using local path
+        '/absolute/path/to/custom-reporter.cjs',
+      ],
+    },
   },
   plugins: [
     // This is required to build the test files with SWC
