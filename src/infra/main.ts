0a6c278471583af0c19624b64ed63acd838af0a0
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-08-27 19:02:56 -0300

    adicionando documentação swagger

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,11 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { EnvService } from './env/env.service';
+import {
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const env = app.get(EnvService);
   const port = env.get('PORT');
+
+  const config = new DocumentBuilder()
+    .setTitle('nest clean')
+    .setDescription('nest clean api description endpoints')
+    .setVersion('1.0')
+    .addTag('forum')
+    .build();
+
+  const options: SwaggerDocumentOptions = {};
+
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('api', app, document, {
+    jsonDocumentUrl: 'api/json',
+  });
 
   await app.listen(port);
 }
