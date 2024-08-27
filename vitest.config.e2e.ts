bc936ef13028ea6b33106e99c233a3293bf53750
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-08-27 19:03:14 -0300

    chore: Update vitest.config.e2e.ts for test coverage reporting

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -1,14 +1,17 @@
 import swc from 'unplugin-swc';
 import { defineConfig } from 'vitest/config';
-import tsConfigPaths from 'vitest-tsconfig-paths'
+import tsConfigPaths from 'vitest-tsconfig-paths';
 
 export default defineConfig({
   test: {
     include: ['**/*.e2e-spec.ts'],
     globals: true,
     root: './',
-    setupFiles:['./test/setup-e2e.ts']
-  },  
+    setupFiles: ['./test/setup-e2e.ts'],
+    coverage: {
+      provider: 'v8',
+      reportsDirectory: './test/e2e/coverage',
+    },
+  },
   plugins: [swc.vite(), tsConfigPaths()],
-  
 });
