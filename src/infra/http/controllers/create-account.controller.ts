c3b31dd7461feb0f01a5c40cfcd8907be2132d29
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-09-01 16:50:00 -0300

    feat: Update CreateAccountController to use CreateAccountDto
    The CreateAccountController has been updated to use the CreateAccountDto instead of the deprecated CreateAccountDTO. This change ensures consistency and follows the established naming convention for DTOs in the project.

--- src/infra/http/controllers/create-account.controller.ts
+++ src/infra/http/controllers/create-account.controller.ts
@@ -13,7 +13,8 @@ import { ZodValidationPipe } from '../pipes/zod-validation-pipe';
 import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student';
 import { StudentAlreadyExistsError } from '@/domain/forum/application/use-cases/erros/student-already-exists-error';
 import { Public } from '@/infra/auth/public';
-import { CreateAccountDTO } from '../dto/create-account-dto';
+import { CreateAccountDto } from '../dto/create-account-dto';
+import { ApiBody } from '@nestjs/swagger';
 
 const createAccountBodySchema = z.object({
   name: z.string(),
@@ -21,15 +22,18 @@ const createAccountBodySchema = z.object({
   password: z.string().min(6),
 });
 
+type CreateAccountBody = z.infer<typeof createAccountBodySchema>;
+
 @Public()
 @Controller('/accounts')
 export class CreateAccountController {
   constructor(private registerStudent: RegisterStudentUseCase) {}
 
   @Post()
+  @ApiBody({ type: CreateAccountDto })
   @HttpCode(201)
   @UsePipes(new ZodValidationPipe(createAccountBodySchema))
-  async handler(@Body() body: CreateAccountDTO) {
+  async handler(@Body() body: CreateAccountDto) {
     const { name, email, password } = body;
     const result = await this.registerStudent.execute({
       name,
