77474d792acffcabf9a281ea67ab6c3588358df0
Henry <henry.arcaya@sptech.school>
2025-06-19 13:45:27 -0300

    criando repositoro de cache

--- src/infra/cache/cahce-repository.ts
+++
@@ -1,5 +0,0 @@
-export abstract class CacheRepository {
-  abstract get(key: string): Promise<string>;
-  abstract set(key: string, value: string): Promise<void>;
-  abstract delete(key: string): Promise<void>;
-}
